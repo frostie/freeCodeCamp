1ea7268aa72937f1972edc89419dd0bc6cba1dc8
Phil Marcoccia <36013742+frostie@users.noreply.github.com>
2019-09-25 09:26:26 -0400

    Create Finding a remainder.js

---
+++ JavaScript/Finding a remainder.js
@@ -0,0 +1,8 @@
+/* In mathematics, a number can be checked to be even or odd by checking the remainder of the division of the number by 2.
+
+17 % 2 = 1 (17 is Odd)
+48 % 2 = 0 (48 is Even)
+
+*/
+
+var remainder = 11 % 3;
