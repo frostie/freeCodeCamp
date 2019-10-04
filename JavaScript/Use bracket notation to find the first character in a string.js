1c3ee35fd78ac1edd63cca147874035b03cf50a3
Phil Marcoccia <36013742+frostie@users.noreply.github.com>
2019-10-04 16:49:49 -0400

    Create Use bracket notation to find the first character in a string.js

---
+++ JavaScript/Use bracket notation to find the first character in a string.js
@@ -0,0 +1,8 @@
+/* The character at index 0 in the word "Charles" is "C". 
+So if var firstName = "Charles", you can get the value 
+of the first letter of the string by using firstName[0]. */
+
+var firstLetterOfLastName = "";
+var lastName = "Lovelace";
+
+firstLetterOfLastName = lastName[0];
