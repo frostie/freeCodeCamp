145035b23164aa09f47cabbfba1d66e4d577476c
Phil Marcoccia <36013742+frostie@users.noreply.github.com>
2019-10-04 16:28:00 -0400

    Create Escape sequences in strings.js

---
+++ JavaScript/Escape sequences in strings.js
@@ -0,0 +1,7 @@
+/* 2 reasons to use escaping characters: (1) To allow you 
+to use characters you might not otherwise be able to type out, (e.g., a 
+backspace), and (2) To allow you to represent multiple quotes in a string 
+without JavaScript misinterpreting what you mean. */
+
+var myStr = "FirstLine\n\tSecondLine\nThirdLine";
+
