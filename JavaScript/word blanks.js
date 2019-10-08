9476eb977d7a415c4eb095c5cceb235ee51d5ed0
Phil Marcoccia <36013742+frostie@users.noreply.github.com>
2019-10-08 17:06:00 -0400

    Create word blanks.js

---
+++ JavaScript/word blanks.js
@@ -0,0 +1,6 @@
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+  var result = "";
+  result+= "My "+myAdjective+" "+myNoun+" "+myVerb+" very "+myAdverb+".";
+  return result;
+}
+wordBlanks("dog", "big", "ran", "quickly");
