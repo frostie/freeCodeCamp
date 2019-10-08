1644e7e92f2ad82ea682d5868820984f35a61170
Phil Marcoccia <36013742+frostie@users.noreply.github.com>
2019-10-08 17:42:01 -0400

    Create unshift().js

---
+++ JavaScript/unshift().js
@@ -0,0 +1,7 @@
+// Adds the element at the beginning of the array
+
+var myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
+
+// Only change code below this line.
+myArray.unshift(["Paul", 35]);
