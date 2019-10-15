9a68925e97f4311cd38992f71458b3a812238dde
Phil Marcoccia <36013742+frostie@users.noreply.github.com>
2019-10-15 09:24:28 -0400

    Create Logical And operator.js

---
+++ JavaScript/Logical And operator.js
@@ -0,0 +1,12 @@
+/* This is an alternative to a nested if statement */
+
+function testLogicalAnd(val) {
+
+  if (val <= 50 && val >= 25) {
+    return "Yes";
+  }
+
+  return "No";
+}
+
+testLogicalAnd(10);
